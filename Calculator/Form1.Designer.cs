c0320b5aac45fcb79d675c3494fea90b01307d4b
Quynhtran1623 <quynhtrana1k45@gmail.com>
2022-10-29 12:51:47 +0700

    Update Form1

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -29,13 +29,136 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lbKQ = new System.Windows.Forms.Label();
+            this.btCong = new System.Windows.Forms.Button();
+            this.btTru = new System.Windows.Forms.Button();
+            this.btNhan = new System.Windows.Forms.Button();
+            this.btChia = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(12, 9);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(92, 20);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Số thứ nhất";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(12, 58);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(81, 20);
+            this.lb2.TabIndex = 1;
+            this.lb2.Text = "Số thứ hai";
+            // 
+            // lbKQ
+            // 
+            this.lbKQ.AutoSize = true;
+            this.lbKQ.Location = new System.Drawing.Point(12, 115);
+            this.lbKQ.Name = "lbKQ";
+            this.lbKQ.Size = new System.Drawing.Size(67, 20);
+            this.lbKQ.TabIndex = 2;
+            this.lbKQ.Text = "Kết Quả";
+            // 
+            // btCong
+            // 
+            this.btCong.Location = new System.Drawing.Point(97, 272);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(75, 41);
+            this.btCong.TabIndex = 3;
+            this.btCong.Text = "Cộng";
+            this.btCong.UseVisualStyleBackColor = true;
+            // 
+            // btTru
+            // 
+            this.btTru.Location = new System.Drawing.Point(199, 272);
+            this.btTru.Name = "btTru";
+            this.btTru.Size = new System.Drawing.Size(75, 41);
+            this.btTru.TabIndex = 4;
+            this.btTru.Text = "Trừ";
+            this.btTru.UseVisualStyleBackColor = true;
+            // 
+            // btNhan
+            // 
+            this.btNhan.Location = new System.Drawing.Point(306, 272);
+            this.btNhan.Name = "btNhan";
+            this.btNhan.Size = new System.Drawing.Size(75, 41);
+            this.btNhan.TabIndex = 5;
+            this.btNhan.Text = "Nhân";
+            this.btNhan.UseVisualStyleBackColor = true;
+            // 
+            // btChia
+            // 
+            this.btChia.Location = new System.Drawing.Point(431, 272);
+            this.btChia.Name = "btChia";
+            this.btChia.Size = new System.Drawing.Size(75, 41);
+            this.btChia.TabIndex = 6;
+            this.btChia.Text = "Chia";
+            this.btChia.UseVisualStyleBackColor = true;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(112, 6);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 7;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(112, 58);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 8;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(112, 115);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 26);
+            this.txtKQ.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btChia);
+            this.Controls.Add(this.btNhan);
+            this.Controls.Add(this.btTru);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.lbKQ);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lbKQ;
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.Button btTru;
+        private System.Windows.Forms.Button btNhan;
+        private System.Windows.Forms.Button btChia;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txtKQ;
     }
 }
 
---
+++ CalculatorTester/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+
+namespace CalculatorTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculation cal;
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.cal = new Calculation(10, 5);
+        }
+        [TestMethod]
+        public void TestAddOperator()
+        {
+            Assert.AreEqual(cal.Execute("+"), 15);
+        }
+        [TestMethod]
+        public void TestSubOperator()
+        {
+            Assert.AreEqual(cal.Execute("-"), 5);
+        }
+        [TestMethod]
+        public void TestMulOperator()
+        {
+            Assert.AreEqual(cal.Execute("*"), 50);
+        }
+        [TestMethod]
+        public void TestDivOperator()
+        {
+            Assert.AreEqual(cal.Execute("/"), 2);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivByZero()
+        {
+            Calculation c = new Calculation(2, 0);
+            c.Execute("/");
+        }
+        [TestMethod]
+        public void TestDivRound()
+        {
+            Calculation c = new Calculation(5, 3);
+            Assert.AreEqual(c.Execute("/"), 2);
+        }
+
+    }
+
+
+    internal class Calculation
+    {
+        public Calculation(int v1, int v2)
+        {
+        }
+
+        internal int Execute(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
